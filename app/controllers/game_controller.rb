be7111019dd7d15e50e4028fdb645abba847061b
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-25 21:42:24 +0000

    defined rock method

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,4 +2,8 @@ class GameController < ApplicationController
   def rules
     render({ :template => "game_templates/rules"})
   end
+
+  def rock
+    render({ :template => "game_templates/play_rock"})
+  end
 end
